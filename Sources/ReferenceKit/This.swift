56937ed6456da102fd6c043b845067b20450aac5
Jonathan Pappas <jonnyboypappas@gmail.com>
2021-05-12 09:06:38 -0700

    aha

--- Sources/ReferenceKit/This.swift
+++ Sources/ReferenceKit/This.swift
@@ -1,30 +1,30 @@
 
-@propertyWrapper struct Reference<T> {
-    var foo: UnsafeMutablePointer<T>
-    var wrappedValue: UnsafeMutablePointer<T> {
+@propertyWrapper public struct Reference<T> {
+    public var foo: UnsafeMutablePointer<T>
+    public var wrappedValue: UnsafeMutablePointer<T> {
         get { foo }
         set { foo = newValue }
     }
-    init(wrappedValue: UnsafeMutablePointer<T>) { foo = wrappedValue }
+    public init(wrappedValue: UnsafeMutablePointer<T>) { foo = wrappedValue }
 }
 
 
 // Extensions
-extension UnsafeMutablePointer where Pointee: AdditiveArithmetic {
+public extension UnsafeMutablePointer where Pointee: AdditiveArithmetic {
     static func += (lhs: Self, rhs: Pointee) { lhs.pointee += rhs }
     static func += (lhs: Self, rhs: Self) { lhs.pointee += rhs.pointee }
     static func -= (lhs: Self, rhs: Pointee) { lhs.pointee -= rhs }
     static func -= (lhs: Self, rhs: Self) { lhs.pointee -= rhs.pointee }
 }
-extension UnsafeMutablePointer where Pointee: Numeric {
+public extension UnsafeMutablePointer where Pointee: Numeric {
     static func *= (lhs: Self, rhs: Pointee) { lhs.pointee *= rhs }
     static func *= (lhs: Self, rhs: Self) { lhs.pointee *= rhs.pointee }
 }
-extension UnsafeMutablePointer where Pointee: FixedWidthInteger {
+public extension UnsafeMutablePointer where Pointee: FixedWidthInteger {
     static func /= (lhs: Self, rhs: Pointee) { lhs.pointee /= rhs }
     static func /= (lhs: Self, rhs: Self) { lhs.pointee /= rhs.pointee }
 }
-extension UnsafeMutablePointer where Pointee: FloatingPoint {
+public extension UnsafeMutablePointer where Pointee: FloatingPoint {
     static func += (lhs: Self, rhs: Pointee) { lhs.pointee += rhs }
     static func -= (lhs: Self, rhs: Pointee) { lhs.pointee -= rhs }
     static func *= (lhs: Self, rhs: Pointee) { lhs.pointee *= rhs }
@@ -34,12 +34,12 @@ extension UnsafeMutablePointer where Pointee: FloatingPoint {
     static func *= (lhs: Self, rhs: Self) { lhs.pointee *= rhs.pointee }
     static func /= (lhs: Self, rhs: Self) { lhs.pointee /= rhs.pointee }
 }
-extension UnsafeMutablePointer where Pointee == String {
+public extension UnsafeMutablePointer where Pointee == String {
     static func += (lhs: Self, rhs: Pointee) { lhs.pointee += rhs }
     static func += (lhs: Self, rhs: Self) { lhs.pointee += rhs.pointee }
     static func *= (lhs: Self, rhs: Int) { lhs.pointee *= rhs }
 }
-extension String {
+public extension String {
     static func *= (lhs: inout Self, rhs: Int) { lhs = String.init(repeating: lhs, count: rhs) }
 }
 
